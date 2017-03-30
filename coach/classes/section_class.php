a358a7c538ebfb21aed06e75665be70edfd7a7ca
Jordon13 <jgayle@utopiatehc.com>
2017-03-30 09:20:35 -0400

    Update section_class.php

--- coach/classes/section_class.php
+++ coach/classes/section_class.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $ID = $_SESSION["account"];
-require "../../classes/config.php";
+require "../configuration/config.php";
 
 
 class section extends configuration {
@@ -12,7 +12,7 @@ class section extends configuration {
 		?>
 		<ul class="controlPanel">
         	<li onClick="addClass()"><a class="btn">New</a></li>
-            <li><a class="btn" disabled="disabled">Delete</a></li>
+            <li ><a class="btn" disabled="disabled">Delete</a></li>
         </ul>
 		<?php
 		
@@ -30,32 +30,65 @@ class section extends configuration {
 		
 		while($result = $query->fetch_assoc()){
 			$classID = $result['class_ID'];
+            $className = $result['class_name'];
 			?>
             
             	
                 	
-                    <tr>
-                    	<td><input type='checkbox' onselect="selectClass()"></td>
+                    <tr >
+                    	<td ><input type='checkbox' onselect="selectClass()"></td>
                         <td><?php echo $classID;?></td>
-                        <td><?php echo $result['class_name'];?></td>
+                        <td><?php echo $className;?></td>
                         <td>
 							<ul class="controlPanel">
-            <li><a class="btn" disabled="disabled">Delete</a></li>
-            <li><a class="btn">Move up</a></li>
-            <li><a class="btn">Move down</a></li>
-            <li><a class="btn" href="classroomData.php?id=<?php echo $classID;?>" >Edit</a></li>
+                                <!-- href="../../../simms/classes/delete.php?id=<?php// echo $classID;?> -->
+                                
+                        <?php echo "<li ><a href='#'  class='btn delete' id='".$classID."' >delete</a></li>"; ?>       
+                        
+            <!--<li><a class="btn" href="#" id="" >Delete</a></li>-->
+            <?php //echo "<li><a class=\"btn\" href=../../simms/classes/delete.php?id=".$classID.">Delete</a>" ?>
+                <li><a class="btn">Move up</a></li>
+                <li><a class="btn">Move down</a></li>
+                <li><a class="btn" href="classroomData.php?id=<?php echo $classID;?>" >Edit</a></li>
         </ul>
                         </td>
                     </tr>
-                
-				
-                
                
 			<?php	
 			}
 			
 			?>
+                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+                <script type="text/javascript" >
+                
+                    
+                    $(function(){
+                        $(".delete").click(function(){
+                            var element = $(this);
+                            var userid = element.attr("id");
+                            var info = 'id=' + userid;
+                            alert(info);
+                            if(confirm("Are you sure you want to delete")){
+                            $.ajax({
+                            url: '../../simms/coach/configuration/call_del_func.php',
+                            type: 'POST',
+                            data: info,
+                            success: function(data){
+                            
+                        }
+                        });
+                        $(this).parent().parent().parent().parent().fadeOut(2000,function(){
+                            $(this).remove();
+                        });
+                        };
+                      return false;
+                        });
+                    });
+                    
+                </script>
             	</table>
+
+
 						      	
             
             <script>
@@ -77,11 +110,94 @@ class section extends configuration {
 		}
 	public function editAssignment($classID,$edit,$assignDetails,$assignmentName) {
 		
+		if($edit == "assign"){
+			
+			$query = $this->connect->query("UPDATE `class_assignment` SET `title`='$assignmentName',`assignment`='$assignDetails' WHERE `class_ID`='$classID'");
+			
+			$query = $this->connect->query("SELECT * FROM `class_assignment` WHERE `class_ID`='$classID' ");
+			
+			while($result = $query->fetch_assoc()){
+				
+				?>
+					
+                    	<tr>
+                        	<td><?php echo $result['title'];?></td>
+                            <td><?php echo $result['assignment'];?></td>
+                            <td><a class="btn" onclick="ManageAssing('delete','<?php echo $result['class_ID']?>')">Delete</a></td>
+                            <td><a class="btn" onclick="ManageAssing('edit','<?php echo $result['class_ID']?>')">Edit</a></td>
+                        </tr>
+                    
+				<?php
+				
+				}
+				
+				
+				
+			
+			}else{
+			
+			?>
+			<h2>Assignment</h2>
+            <input type="hidden" value="<?php echo $classID;?>" name="ID">
+                    <table>
+                    	<tr>
+                        	<td>Assignment Title:</td>
+                            <td><input type="text" name="assignmentNameEdit"></td>
+                        </tr>
+                        <tr>
+                        
+                            <td colspan="2"><textarea name="assignmentedit" id="editor1"></textarea></td>
+                        </tr>
+                        <tr>
+                        	<td> <a class="btn" style="cursor:pointer" onClick="editAssignment()">Update</a></td>
+                        </tr>
+                       
+                    </table>
+                    <script>
+                    	functon editAssignment() {
+							var id = $("input[name=ID]").val();
+							var name = $("input[name=assignmentNameEdit]").val();
+							var assignment = $("input[name=assignmentedit]").val();
+							
+							$.post("classes/section_class.php",{name:name,classID:id,assignment:assignment,readyassignedit:"assign"},function(data){
+								
+								});
+							}
+							// Replace the <textarea id="editor1"> with a CKEditor
+                // instance, using default configuration.
+                CKEDITOR.replace( 'editor1' );
+                    </script>
+           
+		<?php
+				
+				}
 		
 		
 		
 		}
-	
+	public function DeleteAssignment($classID) {
+		$this->connect->query("DELETE FROM `class_assignment` WHERE `class_ID`='$classID'");
+		
+		$query = $this->connect->query("SELECT * FROM `class_assignment` WHERE `class_ID`='$classID' ");
+			
+			
+			while($result = $query->fetch_assoc()){
+				
+				?>
+					
+                    	<tr>
+                        	<td><?php echo $result['title'];?></td>
+                            <td><?php echo $result['assignment'];?></td>
+                            <td><a class="btn" onclick="ManageAssing('delete','<?php echo $result['class_ID']?>')">Delete</a></td>
+                            <td><a class="btn" onclick="ManageAssing('edit','<?php echo $result['class_ID']?>')">Edit</a></td>
+                        </tr>
+                    
+				<?php
+				
+				}
+				
+				
+		}				
 		
 	public function addClassesExt($section){
 		
@@ -198,3 +314,14 @@ if(isset($editClasses) && !empty($editClasses)){
 if(isset($assignmentName) && !empty($assignmentName)){
 	$section->addAssignment($assignmentName,$assignDetails,$classID,$_SESSION["account"]);
 	}
+if(isset($action) && !empty($action)){
+	if($action == 'delete'){
+		$section->DeleteAssignment($classID);
+		}else{
+		$section->editAssignment($classID,$edit,$assignDetails,$assignmentName);	
+			}
+	}					
+if(isset($edit) && !empty($edit)){
+	$section->editAssignment($classID,$edit,$assignDetails,$assignmentName);
+	}	
+	
